1231e13ecd8dfc8731db534e60255a4115bad8a1
rinoaxyrl <almodovarrinoaxyrel73@gmail.com>
2024-05-20 22:04:53 +0800

    modified the app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
+  <div id="app">
   <!-- the router view is where all the routes are rendered -->
   <router-view/>
+  <CreatePage />
+</div>
 </template>
 
 <script>
+import CreatePage from './components/CreatePage.vue';
 export default {
   name: 'App',
+  components: {
+    CreatePage
+  },
   data() {
     return {
       // assign in root component to easily change the api url in production environment
