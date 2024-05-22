8735f2e3e25186075d227c34e1f16261e8cafce2
rinoaxyrl <almodovarrinoaxyrel73@gmail.com>
2024-05-22 18:47:45 +0800

    modified the create page file

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -33,9 +33,43 @@
   </template>
   
   <script>
+  import axios from 'axios';
   export default {
-    name: 'CreatePost'
+    name: 'CreatePost',
+    data() {
+    return {
+      title: '',
+      body: '',
+      errors: {},
+      posts: [],
+    };
+  },
+  methods: {
+    async createPost() {
+      try {
+        const response = await axios.post(this.$root.$data.apiUrl + '/create', {
+          title: this.title,
+          body: this.body,
+        }, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        });
+        if (response.status === 201) {
+          this.posts.push(response.data);
+          alert('Post created successfully!');
+          this.$router.push('/home');
+        }
+
+      } catch (error) {
+        this.errors = error.response.data.errors;
+      }
+    },
+    clearErrors(field) {
+      this.errors[field] = null;
+    }
   }
+}
   </script>
 
   
